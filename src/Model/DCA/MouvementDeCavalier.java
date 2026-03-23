72c62e728b5331ab7c76120b0d29c83f8989f93c
Hugo LOREAU <hugo.loreau@etu.univ-lyon1.fr>
2026-03-23 14:28:41 +0100

    syntax changes

--- src/Model/DCA/MouvementDeCavalier.java
+++ src/Model/DCA/MouvementDeCavalier.java
@@ -4,7 +4,8 @@ import Model.Piece.Piece;
 import java.util.List;
 
 // La cartouche "Mouvement en L"
-public class MouvementDeCavalier extends DecorateurMouvement {
+public class MouvementDeCavalier extends DecorateurMouvement
+{
 
     public MouvementDeCavalier(CalculateurMouvement m)
     {
@@ -12,7 +13,8 @@ public class MouvementDeCavalier extends DecorateurMouvement {
     }
 
     @Override
-    public List<int[]> getCoups(int row, int col, Piece[][] board, boolean isBlanc) {
+    public List<int[]> getCoups(int row, int col, Piece[][] board, boolean isBlanc)
+    {
         List<int[]> coups = super.getCoups(row, col, board, isBlanc);
 
         int[][] sauts =
@@ -24,18 +26,14 @@ public class MouvementDeCavalier extends DecorateurMouvement {
         for (int[] s : sauts) {
             int r = row + s[0];
             int c = col + s[1];
-
-            // Si le saut atterrit bien sur le plateau...
             if (r >= 0 && r < 8 && c >= 0 && c < 8) {
-                // Et que la case est vide OU contient un ennemi...
-                if (board[r][c] == null || board[r][c].getCouleur() != isBlanc) {
-                    // On ajoute ce coup à notre carnet !
+                if (board[r][c] == null || board[r][c].getCouleur() != isBlanc)
+                {
                     coups.add(new int[]{r, c});
                 }
             }
         }
 
-        // 4. On renvoie le carnet mis à jour
         return coups;
     }
 }
